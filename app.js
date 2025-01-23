27f61f631837cd04542a12c5d548f11bb8a5867c
enricoricciardella <enrico.ricciardella@gmail.com>
2025-01-23 18:15:29 +0100

    feat: aggiunta show + immagini e middleware

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+app.use(express.json());
 //importo il routers
 const router = require("./routes/movieRoutes");
 //aggiungo la rotta /movies all'URL
 app.use("/movies", router);
 
+// Middleware per rotte inesistenti
+app.use((req, res, next) => {
+  res.status(404).send("Pagina non trovata... RIP");
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -6,23 +6,33 @@ function Index(req, res) {
     const sql = `SELECT * FROM movies`;
     connection.query(sql, (err, movies) => {
         if (err) {
-          return next(new Error(err.message));
+            return next(new Error(err.message));
         }
-    
+
         return res.status(200).json({
-          status: "success",
-          data: movies,
+            status: "success",
+            data: movies,
         });
-      });
-    
+    });
 };
-
+//ENDPOINT SHOW
 function show(req, res) {
-    
+    const urlId = req.params.id; 
+    const sql = `SELECT * FROM movies WHERE id = ?`;
+    connection.query(sql, [urlId], (err, movies) => {
+        if (err) {
+            return next(new Error(err.message));
+        }
+
+        return res.status(200).json({
+            status: "success",
+            data: movies,
+        });
+    });
 };
 
 
 
 
 
-module.exports = {Index, show};
+module.exports = { Index, show };
